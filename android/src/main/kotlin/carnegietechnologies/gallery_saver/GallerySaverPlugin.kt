76c71ce4e7824dac00b6263c90c4e15285e24616
Emilio Maciel Octopus <emilio.maciel2@gmail.com>
2025-06-05 15:58:20 +0200

    commiting tries...

--- android/src/main/kotlin/carnegietechnologies/gallery_saver/GallerySaverPlugin.kt
+++ android/src/main/kotlin/carnegietechnologies/gallery_saver/GallerySaverPlugin.kt
@@ -8,8 +8,6 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class GallerySaverPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
